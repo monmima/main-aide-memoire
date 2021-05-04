a72ced04c25527d16d3ea9cdca64190d4f406dc3
monmima <monmima@protonmail.com>
2021-05-03 22:46:43 -0400

    added two new tutorials to this repository

---
+++ php/php-tutorial-by-brad-traversy/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>This is the root page in the php-tutorial-by-brad-traversy folder.</p>
+
+    <p>To run this page in a PHP server, start Xampp and then type "localhost/php-tutorial-by-brad-traversy" in the adress bar of your browser.</p>
+
+    <ol>
+        <li><a title="Part 001" href="part-001.php">Part 001</a></li>
+        <li>No part 2; Brad just shows you how to install Xampp here</li>
+        <li><a title="Part 003" href="part-003.php">Part 003: Variables</a></li>
+        <li><a title="Part 004" href="part-004.php">Part 004: Arrays</a></li>
+        <li><a title="Part 005" href="part-005.php">Part 005: Loops</a></li>
+        <li><a title="Part 006" href="part-006.php">Part 006: Functions</a></li>
+        <li><a title="Part 007" href="part-007.php">Part 007: Conditionals</a></li>
+        <li><a title="Part 008" href="part-008.php">Part 008: Dates</a></li>
+        <li><a title="Part 009" href="part-009-includes-and-requires/index.php">Part 009: Includes and Requires</a></li>
+        <li><a title="Part 010" href="part-010/index.php">Part 010: Server Superglobal</a></li>
+        <li><a title="Part 011" href="part-011.php">Part 011: POST and GET</a></li>
+        <li><a title="Part 012" href="part-012.php">Part 012: Common String Functions</a></li>
+        <li><a title="Part 013" href="part-013.php">Part 013: Ternary and Shorthand Syntax</a></li>
+        <li><a title="Part 014" href="part-014.php">Part 014: Filters and Validations</a></li>
+        <li><a title="Part 015" href="part-015.php">Part 015: PHP Contact Form</a></li>
+        <li><a title="Part 016" href="part-016/index.php">Part 016: Sessions</a></li>
+        <li><a title="Part 017" href="part-017/index.php">Part 017: Cookies</a></li>
+        <li><a title="Part 018" href="part-018/index.php">Part 018: PHP and AJAX</a></li>
+        <li><a title="Part 019" href="part-019/index.php">Part 019: File System Functions</a></li>
+        <li><a title="Part 020" href="part-020.php">Part 020: OOP</a></li>
+        <li><a title="Part 021" href="part-021/index.php">Part 021: Fetching Data (MySQL)</a></li>
+        <li><a title="Part 022" href="part-022/index.php">Part 022: Fetching Data (second part)</a></li>
+    </ol>
+</body>
+</html>
