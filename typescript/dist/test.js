bee2b467e685ac7461459a50dbbd0ff6a08642b8
monmima <monmima@protonmail.com>
2021-12-04 19:36:11 -0500

    add typescript/ folder

---
+++ typescript/dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+// console.log("test");
+let id = 5;
+let id2 = 5;
+// id = "5";
+// typescript does type inference
+///
---
+++ typescript/src/test.ts
@@ -0,0 +1,12 @@
+// console.log("test");
+
+let id: number = 5;
+let id2: number = 5;
+
+
+// id = "5";
+
+// typescript does type inference
+
+///
+
